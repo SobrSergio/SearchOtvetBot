afa82d4aa11dc5c248589f646256aa8d45742968
SergeyM <107222527+SobrSergio@users.noreply.github.com>
2023-10-01 21:41:35 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import logging
 
 storage = MemoryStorage() #хранение памяти 
-bot = Bot(token="5682643007:AAE_AMWsw3kOxMrI3lF0uDlGhVKxSiJeHtc") # Объект бота
+bot = Bot(token="") # Объект бота
 dp = Dispatcher(bot, storage=storage) # Диспетчер
 
 logging.basicConfig(level=logging.INFO) # Включаем логирование, чтобы не пропустить важные сообщения
